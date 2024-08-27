10c689d3680f733c54a10cfb0101f308136810fa
gram <git@orsinium.dev>
2024-08-27 10:58:18 +0200

    add Noise

--- src/processors.rs
+++ src/processors.rs
@@ -438,3 +438,35 @@ impl Processor for Sine {
         Some(Frame::mono(s))
     }
 }
+
+/// Generate a white noise
+pub struct Noise {
+    seed: u32,
+}
+
+impl Noise {
+    pub fn new(seed: u32) -> Self {
+        Self { seed }
+    }
+}
+
+impl Processor for Noise {
+    fn process_children(&mut self, _cn: &mut Vec<Node>) -> Option<Frame> {
+        let mut samples = [0f32; 8];
+        for sample in samples.iter_mut() {
+            let mut x = self.seed;
+            if x == 0 {
+                x = 1;
+            }
+            // xorshift RNG algorithm
+            x ^= x << 13;
+            x ^= x >> 17;
+            x ^= x << 5;
+            self.seed = x;
+            *sample = x as f32;
+        }
+        let samples = Sample::new(samples);
+        let samples = samples / u32::MAX as f32;
+        Some(Frame::mono(samples))
+    }
+}
