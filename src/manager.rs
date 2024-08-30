18c3182948ae41be4bab140e468b5ce44d4d6c8e
gram <git@orsinium.dev>
2024-08-30 20:47:49 +0200

    support a single buffer in Manager.write

--- src/manager.rs
+++ src/manager.rs
@@ -11,28 +11,30 @@ impl Manager {
         }
     }
 
-    pub fn write(&mut self, buf_left: &mut [f32], buf_right: &mut [f32]) {
-        debug_assert_eq!(buf_left.len() % 8, 0);
-        if !buf_right.is_empty() {
-            debug_assert_eq!(buf_left.len(), buf_right.len())
-        }
-        buf_left.fill(0.);
-        buf_right.fill(0.);
-        let mut buf_left = buf_left;
-        let mut buf_right = buf_right;
-
-        while buf_left.len() >= 8 {
+    pub fn write(&mut self, buf: &mut [f32]) {
+        debug_assert_eq!(buf.len() % 16, 0);
+        let mut buf = buf;
+        while buf.len() >= 8 {
             let Some(frame) = self.root.next_frame() else {
+                // fill the remainder of the buffer with zeros
+                // to avoid playing old values
+                buf.fill(0.);
                 break;
             };
-            buf_left[..8].copy_from_slice(&frame.left.as_array_ref()[..]);
-            buf_left = &mut buf_left[8..];
-            if buf_right.len() >= 8 {
-                if let Some(right) = &frame.right {
-                    buf_right[..8].copy_from_slice(&right.as_array_ref()[..]);
-                }
-                buf_right = &mut buf_right[8..];
+
+            // write left
+            let left = &frame.left.as_array_ref()[..];
+            buf[..8].copy_from_slice(left);
+            buf = &mut buf[8..];
+
+            // write right
+            if let Some(right) = &frame.right {
+                let right = &right.as_array_ref()[..];
+                buf[..8].copy_from_slice(right);
+            } else {
+                buf[..8].copy_from_slice(left);
             }
+            buf = &mut buf[8..];
         }
     }
 }
