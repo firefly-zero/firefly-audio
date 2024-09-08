47d3b09f77db410deaed3adb9fab327efa1de3ed
gram <git@orsinium.dev>
2024-09-08 11:35:06 +0200

    test LFOs

--- src/lfo.rs
+++ src/lfo.rs
@@ -2,7 +2,7 @@
 
 /// A Low-Frequency Oscillator. Used for modulation.
 pub trait LFO {
-    fn get(&mut self, now: u32) -> f32;
+    fn get(&self, now: u32) -> f32;
 }
 
 /// Emit first value before the given moment and the second value after.
@@ -19,7 +19,7 @@ impl Switch {
 }
 
 impl LFO for Switch {
-    fn get(&mut self, now: u32) -> f32 {
+    fn get(&self, now: u32) -> f32 {
         if now < self.time {
             self.v1
         } else {
@@ -47,7 +47,7 @@ impl RampUp {
 }
 
 impl LFO for RampUp {
-    fn get(&mut self, now: u32) -> f32 {
+    fn get(&self, now: u32) -> f32 {
         if now <= self.start_at {
             return self.start;
         }
@@ -58,7 +58,43 @@ impl LFO for RampUp {
         if duration == 0 {
             return self.end;
         }
-        let ratio = (duration - now) as f32 / duration as f32;
-        (self.end - self.start) * ratio
+        let elapsed = now - self.start_at;
+        let ratio = elapsed as f32 / duration as f32;
+        self.start + (self.end - self.start) * ratio
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn switch() {
+        let lfo = Switch::new(2., 4., 10);
+        assert_eq!(lfo.get(0), 2.);
+        assert_eq!(lfo.get(6), 2.);
+        assert_eq!(lfo.get(9), 2.);
+
+        assert_eq!(lfo.get(10), 4.);
+        assert_eq!(lfo.get(11), 4.);
+        assert_eq!(lfo.get(12), 4.);
+        assert_eq!(lfo.get(21), 4.);
+        assert_eq!(lfo.get(100), 4.);
+    }
+
+    #[test]
+    fn ramp_up() {
+        let lfo = RampUp::new(2., 4., 10, 20);
+        assert_eq!(lfo.get(0), 2.);
+        assert_eq!(lfo.get(8), 2.);
+        assert_eq!(lfo.get(10), 2.);
+
+        assert_eq!(lfo.get(20), 4.);
+        assert_eq!(lfo.get(23), 4.);
+        assert_eq!(lfo.get(100), 4.);
+
+        assert_eq!(lfo.get(13), 2.6);
+        assert_eq!(lfo.get(15), 3.);
+        assert_eq!(lfo.get(17), 3.4);
     }
 }
