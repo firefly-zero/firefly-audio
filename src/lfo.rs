3603aeb5a738baa34463b310962486c18dcebccd
gram <git@orsinium.dev>
2024-09-08 11:20:54 +0200

    add RampUp LFO

--- src/lfo.rs
+++ src/lfo.rs
@@ -2,40 +2,63 @@
 
 /// A Low-Frequency Oscillator. Used for modulation.
 pub trait LFO {
-    fn reset(&mut self);
-    fn get(&mut self, now: u32) -> Option<f32>;
+    fn get(&mut self, now: u32) -> f32;
 }
 
-/// Emit the given value once at the given time.
-pub struct Once {
-    value: f32,
+/// Emit first value before the given moment and the second value after.
+pub struct Switch {
+    v1: f32,
+    v2: f32,
     time: u32,
-    emitted: bool,
 }
 
-impl Once {
-    pub fn new(value: f32, time: u32) -> Self {
-        Self {
-            value,
-            time,
-            emitted: false,
-        }
+impl Switch {
+    pub fn new(v1: f32, v2: f32, time: u32) -> Self {
+        Self { v1, v2, time }
     }
 }
 
-impl LFO for Once {
-    fn reset(&mut self) {
-        self.emitted = false
-    }
-
-    fn get(&mut self, now: u32) -> Option<f32> {
-        if self.emitted {
-            return None;
-        }
+impl LFO for Switch {
+    fn get(&mut self, now: u32) -> f32 {
         if now < self.time {
-            return None;
+            self.v1
+        } else {
+            self.v2
         }
-        self.emitted = true;
-        Some(self.value)
+    }
+}
+
+pub struct RampUp {
+    start: f32,
+    end: f32,
+    start_at: u32,
+    end_at: u32,
+}
+
+impl RampUp {
+    pub fn new(start: f32, end: f32, start_at: u32, end_at: u32) -> Self {
+        Self {
+            start,
+            end,
+            start_at,
+            end_at,
+        }
+    }
+}
+
+impl LFO for RampUp {
+    fn get(&mut self, now: u32) -> f32 {
+        if now <= self.start_at {
+            return self.start;
+        }
+        if now >= self.end_at {
+            return self.end;
+        }
+        let duration = self.end_at.saturating_sub(self.start_at);
+        if duration == 0 {
+            return self.end;
+        }
+        let ratio = (duration - now) as f32 / duration as f32;
+        (self.end - self.start) * ratio
     }
 }
