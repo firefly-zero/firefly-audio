15751c29264f2ea81a40e29b7acbec536e0a7ccb
gram <git@orsinium.dev>
2024-10-20 16:07:38 +0200

    use signed integers

--- src/pcm.rs
+++ src/pcm.rs
@@ -21,10 +21,10 @@ impl Display for PcmError {
 /// Play audio from a pulse-code modulated audio file.
 pub struct Pcm<R: embedded_io::Read> {
     reader: R,
-    sample_rate: u16,
+    _sample_rate: u16,
     is16: bool,
     stereo: bool,
-    adpcm: bool,
+    _adpcm: bool,
 }
 
 impl<R: embedded_io::Read> Pcm<R> {
@@ -48,10 +48,10 @@ impl<R: embedded_io::Read> Pcm<R> {
         }
         Ok(Self {
             reader,
-            sample_rate,
+            _sample_rate: sample_rate,
             stereo: header[1] & 0b_100 != 0,
             is16: header[1] & 0b_010 != 0,
-            adpcm: header[1] & 0b_001 != 0,
+            _adpcm: header[1] & 0b_001 != 0,
         })
     }
 }
@@ -63,30 +63,30 @@ impl<R: embedded_io::Read> Processor for Pcm<R> {
             (false, false) => {
                 let mut buf = [0u8; 8];
                 self.reader.read_exact(&mut buf).ok()?;
-                let s = Sample::new(u8s_to_f32s(buf));
+                let s = Sample::new(i8s_to_f32s(buf));
                 Frame::mono(s)
             }
             // 8 bit stereo
             (false, true) => {
                 let mut buf = [0u8; 16];
                 self.reader.read_exact(&mut buf).ok()?;
-                let left = Sample::new(u8s_to_f32s_left(buf));
-                let right = Sample::new(u8s_to_f32s_right(buf));
+                let left = Sample::new(i8s_to_f32s_left(buf));
+                let right = Sample::new(i8s_to_f32s_right(buf));
                 Frame::stereo(left, right)
             }
             // 16 bit mono
             (true, false) => {
                 let mut buf = [0u8; 16];
                 self.reader.read_exact(&mut buf).ok()?;
-                let s = Sample::new(u16s_to_f32s(buf));
+                let s = Sample::new(i16s_to_f32s(buf));
                 Frame::mono(s)
             }
             // 16 bit stereo
             (true, true) => {
                 let mut buf = [0u8; 32];
                 self.reader.read_exact(&mut buf).ok()?;
-                let left = Sample::new(u16s_to_f32s_left(buf));
-                let right = Sample::new(u16s_to_f32s_right(buf));
+                let left = Sample::new(i16s_to_f32s_left(buf));
+                let right = Sample::new(i16s_to_f32s_right(buf));
                 Frame::stereo(left, right)
             }
         };
@@ -94,89 +94,91 @@ impl<R: embedded_io::Read> Processor for Pcm<R> {
     }
 }
 
-fn u8s_to_f32s(us: [u8; 8]) -> [f32; 8] {
+fn i8s_to_f32s(us: [u8; 8]) -> [f32; 8] {
     [
-        u8_to_f32(us[0]),
-        u8_to_f32(us[1]),
-        u8_to_f32(us[2]),
-        u8_to_f32(us[3]),
-        u8_to_f32(us[4]),
-        u8_to_f32(us[5]),
-        u8_to_f32(us[6]),
-        u8_to_f32(us[7]),
+        i8_to_f32(us[0]),
+        i8_to_f32(us[1]),
+        i8_to_f32(us[2]),
+        i8_to_f32(us[3]),
+        i8_to_f32(us[4]),
+        i8_to_f32(us[5]),
+        i8_to_f32(us[6]),
+        i8_to_f32(us[7]),
     ]
 }
 
-fn u8s_to_f32s_left(us: [u8; 16]) -> [f32; 8] {
+fn i8s_to_f32s_left(us: [u8; 16]) -> [f32; 8] {
     [
-        u8_to_f32(us[0]),
-        u8_to_f32(us[2]),
-        u8_to_f32(us[4]),
-        u8_to_f32(us[6]),
-        u8_to_f32(us[8]),
-        u8_to_f32(us[10]),
-        u8_to_f32(us[12]),
-        u8_to_f32(us[14]),
+        i8_to_f32(us[0]),
+        i8_to_f32(us[2]),
+        i8_to_f32(us[4]),
+        i8_to_f32(us[6]),
+        i8_to_f32(us[8]),
+        i8_to_f32(us[10]),
+        i8_to_f32(us[12]),
+        i8_to_f32(us[14]),
     ]
 }
 
-fn u8s_to_f32s_right(us: [u8; 16]) -> [f32; 8] {
+fn i8s_to_f32s_right(us: [u8; 16]) -> [f32; 8] {
     [
-        u8_to_f32(us[1]),
-        u8_to_f32(us[3]),
-        u8_to_f32(us[5]),
-        u8_to_f32(us[7]),
-        u8_to_f32(us[9]),
-        u8_to_f32(us[11]),
-        u8_to_f32(us[13]),
-        u8_to_f32(us[15]),
+        i8_to_f32(us[1]),
+        i8_to_f32(us[3]),
+        i8_to_f32(us[5]),
+        i8_to_f32(us[7]),
+        i8_to_f32(us[9]),
+        i8_to_f32(us[11]),
+        i8_to_f32(us[13]),
+        i8_to_f32(us[15]),
     ]
 }
 
-fn u16s_to_f32s(us: [u8; 16]) -> [f32; 8] {
+fn i16s_to_f32s(us: [u8; 16]) -> [f32; 8] {
     [
-        u16_to_f32(us[0], us[1]),
-        u16_to_f32(us[2], us[3]),
-        u16_to_f32(us[4], us[5]),
-        u16_to_f32(us[6], us[7]),
-        u16_to_f32(us[8], us[9]),
-        u16_to_f32(us[10], us[11]),
-        u16_to_f32(us[12], us[13]),
-        u16_to_f32(us[14], us[15]),
+        i16_to_f32(us[0], us[1]),
+        i16_to_f32(us[2], us[3]),
+        i16_to_f32(us[4], us[5]),
+        i16_to_f32(us[6], us[7]),
+        i16_to_f32(us[8], us[9]),
+        i16_to_f32(us[10], us[11]),
+        i16_to_f32(us[12], us[13]),
+        i16_to_f32(us[14], us[15]),
     ]
 }
 
-fn u16s_to_f32s_left(us: [u8; 32]) -> [f32; 8] {
+fn i16s_to_f32s_left(us: [u8; 32]) -> [f32; 8] {
     [
-        u16_to_f32(us[0], us[1]),
-        u16_to_f32(us[4], us[5]),
-        u16_to_f32(us[8], us[9]),
-        u16_to_f32(us[12], us[13]),
-        u16_to_f32(us[16], us[17]),
-        u16_to_f32(us[20], us[21]),
-        u16_to_f32(us[24], us[25]),
-        u16_to_f32(us[28], us[29]),
+        i16_to_f32(us[0], us[1]),
+        i16_to_f32(us[4], us[5]),
+        i16_to_f32(us[8], us[9]),
+        i16_to_f32(us[12], us[13]),
+        i16_to_f32(us[16], us[17]),
+        i16_to_f32(us[20], us[21]),
+        i16_to_f32(us[24], us[25]),
+        i16_to_f32(us[28], us[29]),
     ]
 }
 
-fn u16s_to_f32s_right(us: [u8; 32]) -> [f32; 8] {
+fn i16s_to_f32s_right(us: [u8; 32]) -> [f32; 8] {
     [
-        u16_to_f32(us[2], us[3]),
-        u16_to_f32(us[6], us[7]),
-        u16_to_f32(us[10], us[11]),
-        u16_to_f32(us[14], us[15]),
-        u16_to_f32(us[18], us[19]),
-        u16_to_f32(us[22], us[23]),
-        u16_to_f32(us[26], us[27]),
-        u16_to_f32(us[30], us[31]),
+        i16_to_f32(us[2], us[3]),
+        i16_to_f32(us[6], us[7]),
+        i16_to_f32(us[10], us[11]),
+        i16_to_f32(us[14], us[15]),
+        i16_to_f32(us[18], us[19]),
+        i16_to_f32(us[22], us[23]),
+        i16_to_f32(us[26], us[27]),
+        i16_to_f32(us[30], us[31]),
     ]
 }
 
-fn u8_to_f32(u: u8) -> f32 {
-    f32::from(u) / f32::from(u8::MAX) * 2. - 1.
+fn i8_to_f32(u: u8) -> f32 {
+    #[allow(clippy::cast_possible_wrap)]
+    let i = u as i8;
+    f32::from(i) / f32::from(u8::MAX) * 2. - 1.
 }
 
-fn u16_to_f32(l: u8, r: u8) -> f32 {
-    let u = u16::from_le_bytes([l, r]);
-    f32::from(u) / f32::from(u16::MAX) * 2. - 1.
+fn i16_to_f32(l: u8, r: u8) -> f32 {
+    let i = i16::from_le_bytes([l, r]);
+    f32::from(i) / f32::from(u16::MAX) * 2. - 1.
 }
